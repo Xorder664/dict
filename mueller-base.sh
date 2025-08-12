9ba9b6636c000d3e55715490e58983fcdc496c51
Dmitriy Tolstikov <dima@MacBook-Pro-Dmitriy.local>
2025-08-12 14:15:42 +0300

    fix patter

--- mueller-base.sh
+++ mueller-base.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # 
-WORD=`echo $1 | sed -e 's/[A-Z]*/\L&/g;s/[,.;:]//g'`;
+WORD=`echo $1 | sed -e 's/[A-Z]*/&/g;s/[,.;:]//g'`;
 if [ -z $WORD ]; then exit; fi;
 LINES=50;  
-DICTIONARY="$HOME/dict/mueller-base.dict.dz"; 
+DICTIONARY="$HOME/Soft/dict/mueller-base.dict.dz"; 
 # 
 zgrep -aA$LINES '^'$WORD'$' $DICTIONARY | sed -e '/^$/d; 1s/^/ &/;/^[^ ]/,$d'
 #
